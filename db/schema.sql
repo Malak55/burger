bf9b0dd7b589c54fa0f370ff7655e38a460294dc
Matt <moldycrow55@gmail.com>
2018-08-18 21:49:11 -0500

    Phase8

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,10 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers(
-  id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(50) NOT NULL,
-  devoured BOOLEAN NOT NULL DEFAULT 0,
-  date TIMESTAMP NOT NULL,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE menu(
-  id INT AUTO_INCREMENT NOT NULL,
-  item VARCHAR(50) NOT NULL,
-  entree BOOLEAN NOT NULL DEFAULT 1,
-  PRIMARY KEY(id)
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false NOT NULL,
+	PRIMARY KEY (id)
 );
